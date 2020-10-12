cae8031aac4e6c8272c077f7baa8225a135e2a7e
Artem Belov <inctnce@icloud.com>
2020-10-13 01:13:43 +0300

    Переопределение цвета текста кнопок

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,22 @@ import App from "./components/App/App";
 import * as serviceWorker from "./utils/serviceWorker";
 import LightTheme from "./assets/Themes/LightTheme";
 import { ThemeProvider } from "@material-ui/core/styles";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const GlobalCss = withStyles({
+  // @global is handled by jss-plugin-global.
+  "@global": {
+    // You should target [class*="MuiButton-root"] instead if you nest themes.
+    ".MuiButton-root": {
+      color: "white",
+    },
+  },
+})(() => null);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={LightTheme}>
+      <GlobalCss />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
