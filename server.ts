e939a91cab2a89cb161eac767ef89a76cb256235
Artem Belov <inctnce@icloud.com>
2020-10-20 16:05:14 +0600

    .

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import favicon from "express-favicon";
+import path from "path";
+const port = process.env.PORT || 8080;
+
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.png"));
+
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+//простой тест сервера
+app.get("/ping", function (_req, res) {
+  return res.send("pong");
+});
+
+//обслуживание html
+app.get("/*", function (_req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
