e4e44672514bbfff1dafc545f845bb8802a0df61
Artem Belov <inctnce@icloud.com>
2020-10-29 20:49:43 +0300

    .

--- src/components/Content/Timetable/List/index.tsx
+++ src/components/Content/Timetable/List/index.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import style from "./style.module.css";
 
-import Cabinet from "../../../../types/Cabinet";
 
 type Props = {
-  selected_cabinets: Cabinet[];
-
-  startDate: Date;
-  endDate: Date;
-
-  getCabinetTimetable: (
-    cabinetIndex: number,
-    cabinetOid: string,
-    startDate: Date,
-    endDate: Date
-  ) => void;
+  // items: any;
+  // startDate: Date;
+  // endDate: Date;
+  // getCabinetTimetable: (
+  //   cabinetIndex: number,
+  //   cabinetOid: string,
+  //   startDate: Date,
+  //   endDate: Date
+  // ) => void;
 };
 
 const TimetableList = (props: Props) => {
-  return <div></div>;
+  
+
+  return (
+    <div>
+    
+    </div>
+  );
 };
 
 export default TimetableList;
