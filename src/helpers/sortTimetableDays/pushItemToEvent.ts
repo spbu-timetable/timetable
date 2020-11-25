2a10b68554f3e50f9fbc238e91834a5db4c16823
Artem Belov <inctnce@icloud.com>
2020-11-25 09:37:15 +0300

    рефакторинг

--- src/helpers/sortTimetableDays/pushItemToEvent.ts
+++ src/helpers/sortTimetableDays/pushItemToEvent.ts
@@ -11,7 +11,7 @@ function pushItemToEvent(event: Event, info: eventInfo): Event {
   event.main.push(info.main);
   event.extra.push(info.extra);
   info.address ? event.address!.push(info.address) : event.address!.push("");
-  info.interval ? (event.interval = info.interval) : (event.interval = event.interval);
+  info.interval ? (event.interval = info.interval) : void 0;
 
   return { ...event };
 }
