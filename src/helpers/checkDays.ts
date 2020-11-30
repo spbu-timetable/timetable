a94232b051ff8bb718d9b0e9b806b09e9ceac6d7
kordebalet232 <yurypetrovich2013@mail.ru>
2020-11-30 17:22:58 +0300

    Проверка пустой недели

--- src/helpers/checkDays.ts
+++ src/helpers/checkDays.ts
@@ -1,14 +1,13 @@
 function checkDays(groupEventsDays: any[]): any[] {
   const weekDays = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
   const checkedGroupEventsDays: any[] = [];
-  
 
   for (let i = 0; i < groupEventsDays.length; i++) {
     const GroupEventDays: any[] = [];
     let k = 6;
     for (let j = 0, y = 0; j < k; y++) {
       console.log(groupEventsDays[i]);
-      if (groupEventsDays[i][j].DayString.includes(weekDays[y])) {
+      if (groupEventsDays[i][j] && groupEventsDays[i][j].DayString.includes(weekDays[y])) {
         GroupEventDays.push(groupEventsDays[i][j]);
         j++;
       } else {
