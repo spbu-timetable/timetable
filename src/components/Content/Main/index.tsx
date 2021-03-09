28b351241191ab59841ae4f2d42ef2daac936d67
Artem Belov <inctnce@icloud.com>
2021-03-09 16:22:08 +0300

    update head

--- src/components/Content/Main/index.tsx
+++ src/components/Content/Main/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import style from "./style.module.css";
 import HugeButton from "../../Reusable/HugeButton";
-import PATH from "../../../PATH";
 
 const Main = () => {
 	return (
 		<div className={style.main}>
 			<h1 className={style.title}>Расписание СПбГУ</h1>
-			<HugeButton title="Кабинеты" link={`${PATH.ADDRESSES}`} />
-			<HugeButton title="Преподаватели" link={`${PATH.EDUCATORS}`} />
-			<HugeButton title="Группы" link={`${PATH.FACULTIES}`} />
+			<HugeButton title="Кабинеты" link="/addresses" />
+			<HugeButton title="Преподаватели" link="/teachers" />
+			<HugeButton title="Группы" link="/faculties" />
 		</div>
 	);
 };
