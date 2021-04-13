984865a750d1b24eeeb176d528a78ca322b13441
Artem Belov <inctnce@icloud.com>
2021-04-13 20:51:20 +0300

    upd layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import AppBar from "@material-ui/core/AppBar/AppBar";
 
 import Toolbar from "@material-ui/core/Toolbar";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import React from "react";
 import style from "./style.module.css";
@@ -9,6 +9,7 @@ import style from "./style.module.css";
 import Picker from "./DatePicker";
 import NavigationDrawer from "./NavigationDrawer";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
 
 type Props = {
 	week: string;
@@ -21,28 +22,26 @@ type Props = {
 
 const Header = (props: Props) => {
 	const history = useHistory();
-	let location = useLocation();
-
-	let isDisabled = false;
-	React.useEffect(() => {
-		if (location.pathname === "/timetable") {
-			isDisabled = true;
-		}
-	});
 
 	return (
 		<AppBar className={style.header} component="header" position={"fixed"} elevation={0} color="primary">
 			<Toolbar>
-				<NavigationDrawer fromDateStr={props.fromDateStr} toDateStr={props.toDateStr} />
+				<Grid container alignItems="center" spacing={3}>
+					<Grid item>
+						<NavigationDrawer fromDateStr={props.fromDateStr} toDateStr={props.toDateStr} />
+					</Grid>
 
-				<Typography
-					className={style.title}
-					onClick={() => {
-						history.push("/");
-					}}
-				>
-					Расписание СПбГУ
-				</Typography>
+					<Grid item>
+						<Typography
+							className={style.title}
+							onClick={() => {
+								history.push("/");
+							}}
+						>
+							Расписание СПбГУ
+						</Typography>
+					</Grid>
+				</Grid>
 			</Toolbar>
 		</AppBar>
 	);
